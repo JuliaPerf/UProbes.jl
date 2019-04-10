e51e72fd81869ff137db3d1fafa695bfd20d8745
Nathan Daly <nhdaly@gmail.com>
2019-04-10 12:51:27 -0400

    Fill in the rest of the libusdt api

--- src/libusdt.jl
+++ src/libusdt.jl
@@ -18,12 +18,14 @@ struct Probe
     probe::Ptr{Probe}
 end
 
+# Consider changing these `Ptr{}`s to `Ref{}`s
 function is_enabled(probe)
-    ccall((:usdt_is_enabled, libusdt), Cint, (Ref{Probe},), probe)
+    ccall((:usdt_is_enabled, libusdt), Cint, (Ptr{Probe},), probe)
 end
 
 function fire_probe(probe, args...)
     # void usdt_fire_probe(usdt_probe_t *probe, size_t argc, void **argv);
+    ccall((:usdt_fire_probe, libusdt), Nothing, (Ptr{Probe}, Csize_t, Ptr{Ptr{Nothing}}), probe, length(args), collect(args))
 end
 
 struct ProbeDef
@@ -36,9 +38,10 @@ struct ProbeDef
     refcnt::Cint
 end
 
-function create_probe(func, name, types...)
+function create_probe(func, name, types::Vector{String})
     # usdt_probedef_t *usdt_create_probe(const char *func, const char *name,
     #                                    size_t argc, const char **types);
+    ccall((:usdt_create_probe, libusdt), Ptr{ProbeDef}, (Cstring, Cstring, Csize_t, Ptr{Ptr{Cchar}}), func, name, length(types), types)
 end
 
 function release(probedef)
@@ -57,31 +60,37 @@ end
 
 function create_provider(name, module_name)
     # usdt_provider_t *usdt_create_provider(const char *name, const char *module);
-    # ccall((:usdt_create_provider, libusdt), Ptr{Provider}, (Cstring, Cstring), name, module_name)
+    ccall((:usdt_create_provider, libusdt), Ptr{Provider}, (Cstring, Cstring), name, module_name)
 end
 function provider_add_probe(provider, probedef)
     # int usdt_provider_add_probe(usdt_provider_t *provider, usdt_probedef_t *probedef);
+    ccall((:usdt_add_probe, libusdt), Cint, (Ptr{Provider}, Ptr{ProbeDef}), provider, probedef)
 end
 function provider_remove_probe(provider, probedef)
     # int usdt_provider_remove_probe(usdt_provider_t *provider, usdt_probedef_t *probedef);
+    ccall((:usdt_remove_probe, libusdt), Cint, (Ptr{Provider}, Ptr{ProbeDef}), provider, probedef)
 end
 function provider_enable(provider)
     # int usdt_provider_enable(usdt_provider_t *provider);
+    ccall((:usdt_provider_enable, libusdt), Nothing, (Ptr{Provider},), provider)
 end
 function provider_disable(provider)
     # int usdt_provider_disable(usdt_provider_t *provider);
+    ccall((:usdt_provider_disable, libusdt), Nothing, (Ptr{Provider},), provider)
 end
 function provider_free(provider)
     # void usdt_provider_free(usdt_provider_t *provider);
+    ccall((:usdt_provider_free, libusdt), Nothing, (Ptr{Provider},), provider)
 end
 
+# TODO: varargs...?
 #function error(provider, error, args...)
 #    # void usdt_error(usdt_provider_t *provider, usdt_error_t error, ...);
 #    ccall((:usdt_error, libusdt), Nothing, (Ptr{Provider}, Cint, typeof(args)...), provider, error, args...);
 #end
 function errstr(provider)
     # char *usdt_errstr(usdt_provider_t *provider);
-    #ccall((:usdt_errstr, libusdt), Cstring, (Ptr{Provider},), provider)
+    ccall((:usdt_errstr, libusdt), Cstring, (Ptr{Provider},), provider)
 end
 
 end
